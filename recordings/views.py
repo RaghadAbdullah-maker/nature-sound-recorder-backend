210cf2583b605e94ab2dc4182f91077b17f33894
raghadabdullah-maker <raghadalhazmi280@gmail.com>
2025-05-01 07:34:06 +0300

    create views for Recording model , CRUD functions

---
+++ recordings/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render,redirect
+from .models import Recording ,Category ,Favorite
+from .serializers import RecordingSerializer,FavoriteSerializer,CategorySerializer
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+
+from django.shortcuts import get_object_or_404
+
+
+# Create your views here.
+
+
+class RecordingCreateView(APIView):
+    
+    
+      def get_permissions(self):
+          if self.request.method == 'Get':
+              return [AllowAny()]
+          return [IsAuthenticated()]
+      
+      def get(self,request):
+          
+          recordings = Recording.objects.all()
+          serializer = RecordingSerializer(recordings,many =True)
+          return Response(serializer.data,status=status.HTTP_200_OK)
+      
+      def post(self,request):
+          
+          serializer = RecordingSerializer(data=request.data)
+          if serializer.is_valid():
+              serializer.save()
+              return Response(serializer.data,status=status.HTTP_201_CREATED)
+          return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+      
+      
+class RecordingDetaitView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def get_object(self, pk):
+        return get_object_or_404(Recording, pk=pk)
+
+
+
+    def get(self, request, pk):
+        recording = self.get_object(pk)
+        serializer = RecordingSerializer(recording)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, pk):
+        recording = self.get_object(pk)
+        recording.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    def patch(self, request, pk): 
+        recording = self.get_object(pk)
+        serializer = RecordingSerializer(recording, data=request.data ,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      
+      
+      
+     
