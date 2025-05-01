4f7067a19062356906866c013964c56a4a069cae
raghadabdullah-maker <raghadalhazmi280@gmail.com>
2025-05-01 07:34:55 +0300

    create views for category model , CRUD functions

--- recordings/views.py
+++ recordings/views.py
@@ -69,4 +69,56 @@ class RecordingDetaitView(APIView):
       
       
       
-     
+      
+
+class CategoryCreateView(APIView):
+    
+      permission_classes = [AllowAny]
+      
+      def get(self,request):     
+       categorys = Category.objects.all()
+       serializer = CategorySerializer(categorys,many =True)
+       return Response(serializer.data,status=status.HTTP_200_OK)
+      
+      
+      def post(self,request):
+          
+          serializer = CategorySerializer(data=request.data)
+          if serializer.is_valid():
+              serializer.save()
+              return Response(serializer.data,status=status.HTTP_201_CREATED)
+          return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+      
+   
+    
+      
+class CategoryDetaitView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+    def get_object(self, pk):
+        return get_object_or_404(Category, pk = pk)
+
+
+
+    def get(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+   
+   
+    def patch(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category, data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      
+      
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+   
