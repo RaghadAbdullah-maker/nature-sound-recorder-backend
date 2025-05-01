7a3113c3a1dc700aafee30b05721d3081cf0eb68
raghadabdullah-maker <raghadalhazmi280@gmail.com>
2025-05-01 07:35:26 +0300

    create views for favirote model , CRUD functions

--- recordings/views.py
+++ recordings/views.py
@@ -122,3 +122,39 @@ class CategoryDetaitView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
     
    
+      
+      
+
+class FavoriteCreateView(APIView):
+    
+      permission_classes = [IsAuthenticated]
+      
+      def get(self,request):
+           
+        favorites = Favorite.objects.filter(user=request.user)
+        serializer = FavoriteSerializer(favorites,many =True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+        
+        
+      def post(self,request):
+          
+          serializer = FavoriteSerializer(data=request.data)
+          if serializer.is_valid():
+              serializer.save(user = request.user)
+              return Response(serializer.data,status=status.HTTP_201_CREATED)
+          return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+      
+   
+    
+      
+class FavoriteDetaitView(APIView):
+    
+    permission_classes = [IsAuthenticated]
+    
+ 
+    def delete(self, request, pk):
+      
+        favorite = get_object_or_404(Favorite,pk=pk , user=request.user )
+        favorite.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
