34b198cb49837b4f03a0a568de70a5a07678442f
raghadabdullah-maker <raghadalhazmi280@gmail.com>
2026-05-12 05:04:08 +0300

    add vercel domain

--- nature_sound_project/settings.py
+++ nature_sound_project/settings.py
@@ -115,8 +115,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+        "https://nature-sound-recorder-frontend.vercel.app",
+
 ]
 CSRF_TRUSTED_ORIGINS = [
+        "https://nature-sound-recorder-frontend.vercel.app",
+
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
