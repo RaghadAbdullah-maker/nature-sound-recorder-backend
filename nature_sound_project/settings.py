02860d124fd242f7e13faa778005786b7dddfd04
raghadabdullah-maker <raghadalhazmi280@gmail.com>
2026-05-22 13:28:42 +0300

    update cors origin

--- nature_sound_project/settings.py
+++ nature_sound_project/settings.py
@@ -115,10 +115,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-       "https://nature-sound-recorder-frontend-e34d-1782jmhkv.vercel.app",
-]
+"https://nature-sound-recorder-frontend-e34d-n23emvcqs.vercel.app",]
 CSRF_TRUSTED_ORIGINS = [
-     "https://nature-sound-recorder-frontend-e34d-1782jmhkv.vercel.app",
+     "https://nature-sound-recorder-frontend-e34d-n23emvcqs.vercel.app",
 
     "http://localhost:5173",
     "http://127.0.0.1:5173",
